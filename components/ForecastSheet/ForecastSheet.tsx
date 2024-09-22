c62d7c1240d371df678ca62e36d3e59755bd7f46
dhunanyan <davit.hunanyan@me.com>
2024-09-22 12:20:21 +0200

    feat: animate forecast scroll

--- components/ForecastSheet/ForecastSheet.tsx
+++ components/ForecastSheet/ForecastSheet.tsx
@@ -1,20 +1,26 @@
 import * as React from "react";
-import { useApplicationDimensions } from "@hooks";
+import { View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 import BottomSheet from "@gorhom/bottom-sheet";
 import { ForecastSheetBackground } from "./ForecastSheetBackground";
 import { ForecastControl } from "./ForecastControl";
 import { ForecastScroll } from "./ForecastScroll";
 import { Separator } from "./Separator";
+import { Widgets } from "../Widgets/Widgets";
 
+import { useApplicationDimensions } from "@hooks";
+import { useForecastSheetPosition } from "@context";
 import { ForecastType } from "@models";
 import { hourly, weekly } from "@data";
 
 import { styles } from "./styles";
-import { Widgets } from "../Widgets/Widgets";
-import { ScrollView } from "react-native-gesture-handler";
-import { useAnimatedReaction, useSharedValue } from "react-native-reanimated";
-import { useForecastSheetPosition } from "@context";
 
 export const ForecastSheet = () => {
   const [selectedForecastType, selectForecastType] =
@@ -36,15 +42,35 @@ export const ForecastSheet = () => {
   const currentPosition = useSharedValue(0);
   const animatedPosition = useForecastSheetPosition();
 
+  const translateXHourly = useSharedValue(0);
+  const translateXWeekly = useSharedValue(width);
+
+  const animatedHourlyStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateXHourly.value }],
+  }));
+  const animatedWeeklyStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateXWeekly.value }],
+  }));
+
+  React.useEffect(() => {
+    if (selectedForecastType === ForecastType.Hourly) {
+      translateXHourly.value = withTiming(0, { duration: 300 });
+      translateXWeekly.value = withTiming(width, { duration: 300 });
+    }
+
+    if (selectedForecastType === ForecastType.Weekly) {
+      translateXHourly.value = withTiming(-width, { duration: 300 });
+      translateXWeekly.value = withTiming(-width, { duration: 300 });
+    }
+  }, [selectedForecastType]);
+
   const normalizePosition = (position: number) => {
     "worklet";
     return ((position - maxY) / (maxY - minY)) * -1;
   };
 
   useAnimatedReaction(
-    () => {
-      return currentPosition.value;
-    },
+    () => currentPosition.value,
     (cv) => {
       animatedPosition.value = normalizePosition(cv);
     }
@@ -70,14 +96,24 @@ export const ForecastSheet = () => {
           contentContainerStyle={{ paddingBottom: 60 }}
           showsVerticalScrollIndicator={false}
         >
-          <ForecastScroll
-            forecastList={
-              selectedForecastType === ForecastType.Hourly ? hourly : weekly
-            }
-            capsuleRadius={capsuleRadius}
-            capsuleHeight={capsuleHeight}
-            capsuleWidth={capsuleWidth}
-          />
+          <View style={styles.forecastScrollsView}>
+            <Animated.View style={[animatedHourlyStyles]}>
+              <ForecastScroll
+                forecastList={hourly}
+                capsuleRadius={capsuleRadius}
+                capsuleHeight={capsuleHeight}
+                capsuleWidth={capsuleWidth}
+              />
+            </Animated.View>
+            <Animated.View style={[animatedWeeklyStyles]}>
+              <ForecastScroll
+                forecastList={weekly}
+                capsuleRadius={capsuleRadius}
+                capsuleHeight={capsuleHeight}
+                capsuleWidth={capsuleWidth}
+              />
+            </Animated.View>
+          </View>
           <Widgets smallWidgetSize={smallWidgetSize} parentWidth={width} />
         </ScrollView>
       </>
--- components/ForecastSheet/styles.ts
+++ components/ForecastSheet/styles.ts
@@ -6,4 +6,7 @@ export const styles = StyleSheet.create({
     height: 5,
     backgroundColor: "rgba(0, 0, 0, 0.3)",
   },
+  forecastScrollsView: {
+    flexDirection: "row",
+  },
 });
